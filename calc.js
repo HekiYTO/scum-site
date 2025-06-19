a7db13b049d917ab5433cef7ff563997c753aa0c
Niktia Skrunnik <138303579+HekiYTO@users.noreply.github.com>
2025-06-19 15:04:27 +0200

    skibidip

--- calc.js
+++ calc.js
@@ -4,11 +4,13 @@ function calculate() {
     let in1 = document.querySelector('.in1');
     let in2 = document.querySelector('.in2');
     let res = document.querySelector('.resultat');
+    let selectedPlane = document.querySelector('input[name="plane"]:checked');
+    let multiplier = Number(selectedPlane.value);
 
     let val1 = Number(in1.value);
     let val2 = Number(in2.value);
 
-    let resu = (((val1*2 + val2*10) - val1 * 2.5) + 20);
+    let resu = (((val1*2 + val2*10) - val1 * 2.5) + 20) * multiplier;
 
     if (resu <= 0) {
     resu = 0;
